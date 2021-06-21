50e4e8767c421a44c40def06bd3186558f730d63
Ilda Neta <iildaemanoely@gmail.com>
2021-06-21 07:22:03 -0300

    feat: Creating the SignIn component

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import { styles } from './styles';
+
+import ButtonIcon from '../../components/ButtonIcon';
+
+import IllustationIMG from '../../assets/illustration.png';
+
+const SignIn = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Image source={IllustationIMG} style={styles.image} />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize{`\n`}suas jogatinas{`\n`}facilmente
+        </Text>
+
+        <Text style={styles.subTitle}>
+          Crie grupos para jogar seus games{`\n`}favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon titleButton="Entrar com Discord" activeOpacity={0.9} />
+      </View>
+    </View>
+  );
+};
+
+export default SignIn;
---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+import { theme } from "../../global/styles/theme";
+import { AlignTypes } from "../../helpers/enums";
+
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.background,
+    flex: 1,
+  },
+
+  image: {
+    width: '100%',
+    height: 360
+  },
+
+  content: {
+    marginTop: -40
+  },
+
+  title: {
+    color: theme.colors.heading,
+    textAlign: AlignTypes.center,
+    fontSize: 40,
+    marginBottom: 16
+  },
+
+  subTitle: {
+    color: theme.colors.heading,
+    fontSize: 15,
+    textAlign: AlignTypes.center,
+    marginBottom: 64
+  }
+})
