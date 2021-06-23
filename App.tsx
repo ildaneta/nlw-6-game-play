d72bb0cd22b4a702189349e4d2c1bccb8e7f5b23
Ilda Neta <iildaemanoely@gmail.com>
2021-06-23 06:49:19 -0300

    feat: Installing external fonts

--- App.tsx
+++ App.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { theme } from './src/global/styles/theme';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Rajdhani_700Bold,
+  Rajdhani_500Medium,
+} from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 
 import SignIn from './src/screens/SignIn';
+import Home from './src/screens/Home'
+import { theme } from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
-      <StatusBar
+     <StatusBar
         barStyle="light-content"
         backgroundColor={theme.colors.background}
       />
-      <SignIn />
+      <Home />
     </>
   );
 }
