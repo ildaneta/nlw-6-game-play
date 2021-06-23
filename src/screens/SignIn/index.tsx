ae9d6883524978597d79aed8b398f51c9cf59768
Ilda Neta <iildaemanoely@gmail.com>
2021-06-23 06:48:21 -0300

    refactor: Excluding the status bar declaration

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
+import { View, Text, Image,  } from 'react-native';
 
 import { styles } from './styles';
 
 import ButtonIcon from '../../components/ButtonIcon';
 
 import IllustationIMG from '../../assets/illustration.png';
-import { theme } from '../../global/styles/theme';
 
 const SignIn = (): JSX.Element => {
   return (
-    <>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor={theme.colors.background}
-      />
       <View style={styles.container}>
         <Image source={IllustationIMG} style={styles.image} />
 
@@ -30,7 +24,6 @@ const SignIn = (): JSX.Element => {
           <ButtonIcon titleButton="Entrar com Discord" activeOpacity={0.9} />
         </View>
       </View>
-    </>
   );
 };
 
