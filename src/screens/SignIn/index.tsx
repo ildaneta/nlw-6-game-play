4322db6691d0535f76b4acfa916136bbb0737e02
Ilda Neta <iildaemanoely@gmail.com>
2021-06-24 13:31:53 -0300

    refactor: Using a function to redirect using a route name

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { View, Text, Image,  } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import { styles } from './styles';
 
 import ButtonIcon from '../../components/ButtonIcon';
 
 import IllustationIMG from '../../assets/illustration.png';
+import { useNavigation } from '@react-navigation/native';
 
 const SignIn = (): JSX.Element => {
+  const navigation = useNavigation();
+
+  const handleRedirectToHome = (routeName: string) => {
+    return navigation.navigate(routeName);
+  };
+
   return (
-      <View style={styles.container}>
-        <Image source={IllustationIMG} style={styles.image} />
+    <View style={styles.container}>
+      <Image source={IllustationIMG} style={styles.image} />
 
-        <View style={styles.content}>
-          <Text style={styles.title}>
-            Conecte-se{'\n'}e organize suas{'\n'}jogatinas
-          </Text>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se{'\n'}e organize suas{'\n'}jogatinas
+        </Text>
 
-          <Text style={styles.subTitle}>
-            Crie grupos para jogar seus games{'\n'}favoritos com seus amigos
-          </Text>
+        <Text style={styles.subTitle}>
+          Crie grupos para jogar seus games{'\n'}favoritos com seus amigos
+        </Text>
 
-          <ButtonIcon titleButton="Entrar com Discord" activeOpacity={0.9} />
-        </View>
+        <ButtonIcon
+          titleButton="Entrar com Discord"
+          onPress={() => handleRedirectToHome('Home')}
+        />
       </View>
+    </View>
   );
 };
 
