e5d63839acda60692a288f5edcd688ec7617ff99
Ilda Neta <iildaemanoely@gmail.com>
2021-07-02 08:24:48 -0300

    refactor: Changing mocks for user data

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useAuth } from '../../hooks/auth';
 
 import Avatar from '../Avatar';
 
 import { styles } from './styles';
 
 const Profile = (): JSX.Element => {
+  const { user } = useAuth();
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://github.com/ildaneta.png" />
+      <Avatar urlImage={user.avatar} />
 
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
 
-          <Text style={styles.userName}>Ilda</Text>
+          <Text style={styles.userName}>{user.firstName}</Text>
         </View>
 
         <Text style={styles.message}>Hoje é dia de vitória</Text>
