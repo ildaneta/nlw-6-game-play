354b5497f5f2796a0e59d059ddd3fe5d6b4d3a0f
Ilda Neta <iildaemanoely@gmail.com>
2021-07-02 08:26:39 -0300

    refactor: Verifying if exists user.id to redirect the routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import AuthRoutes from './auth.routes';
+import { AppRoutes } from './app.routes';
+import { useAuth } from '../hooks/auth';
+
+import SignIn from '../screens/SignIn';
 
 const Routes = (): JSX.Element => {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user.id ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 };
